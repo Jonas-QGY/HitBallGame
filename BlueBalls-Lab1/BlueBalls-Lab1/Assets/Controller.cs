4038ee2bb738eb8d69d06bfc70af1950e9ace1a1
Jonas-QGY <35145130+Jonas-QGY@users.noreply.github.com>
2020-02-06 22:52:11 +0000

    initial Version of LAB practice

---
+++ BlueBalls-Lab1/BlueBalls-Lab1/Assets/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+        Rigidbody r = GetComponent<Rigidbody>();
+        r.velocity = new Vector3(horizontalMovement * speed, -10f, verticalMovement * speed);// -10f added in 06/02 22:00
+    }
+
+}
---
+++ BlueBalls-Lab1/BlueBalls-Lab1/Assets/WallCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCollider : MonoBehaviour
+{
+    public GameObject prototype;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject spawned = Instantiate(prototype, Vector3.Zero);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        //GameObject spawned = Instantiate(prototype, new Vector3(1.5f, 0.15f, 3.0f));
+    }
+}
